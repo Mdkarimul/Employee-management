bc1e3d26f7dcea59b107aec11308bd7903ee2a67
Mdkarimul <7mdkarimul@gmail.com>
2024-09-30 01:02:29 +0530

    commit after completion of employee crud

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,7 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { MasterService } from '../../service/master.service';
-import { IApiResponse, IChildDepartment, IParentDeparment } from '../../model/interface/master';
+import {
+  IApiResponse,
+  IChildDepartment,
+  IParentDeparment,
+} from '../../model/interface/master';
 import { FormsModule } from '@angular/forms';
+import { Employee } from '../../model/class/Employee';
 @Component({
   selector: 'app-employee',
   standalone: true,
@@ -10,17 +15,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './employee.component.scss',
 })
 export class EmployeeComponent implements OnInit {
+
   masterService = inject(MasterService);
   parentDepartmentList = signal<IParentDeparment[]>([]);
   childDepartmentList = signal<IChildDepartment[]>([]);
   parentDeparmentId: number = 0;
-  childDepartmentId:number = 0;
+  childDepartmentId: number = 0;
+  employeeObj: Employee = new Employee();
+  isFormVisible = signal<boolean>(false);
+  employeeList = signal<Employee[]>([]);
 
+  // Constructor function
   constructor() {}
+
+  // Ngoninit function
   ngOnInit(): void {
     this.getParentDeparment();
+    this.getAllEmployee();
   }
 
+  // Get parent department
   getParentDeparment() {
     this.masterService
       .getAllParentDepartment()
@@ -29,16 +43,87 @@ export class EmployeeComponent implements OnInit {
       });
   }
 
+  // Get all child departmentByParentId
   onParentDepartmentChange() {
+    alert(this.parentDeparmentId);
     this.masterService
       .getAllChildDepartmentById(this.parentDeparmentId)
-      .subscribe((result: IApiResponse) => {
+      .subscribe((result: any) => {
+        console.log(result);
         this.childDepartmentList.set(result.data);
       });
   }
-  onChildDepartmentChange(){
-    
+
+  onChildDepartmentChange() {}
+
+  //Create new employee
+  onSave() {
+    this.masterService.saveEmployee(this.employeeObj).subscribe(
+      (data: IApiResponse) => {
+        console.log(data, ' Employee created !');
+        this.getAllEmployee();
+        this.employeeObj = new Employee();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-  isFormVisible = signal<boolean>(false);
+  // Update employee
+  onUpdate(){
+    this.masterService.updateEmployee(this.employeeObj).subscribe({
+      next:(res:Employee)=>{
+        console.log("Employee updated susccessfully !");
+        this.getAllEmployee();
+        this.employeeObj = new Employee();
+      },
+      error:(error)=>{
+    console.log(error);
+      },
+      complete(){
+     console.log('Complete !');
+      }
+    })
+  }
+
+  //Get all employee
+  getAllEmployee() {
+    this.masterService.getAllEmployee().subscribe({
+      next:(res:Employee[])=>{
+        this.employeeList.set(res);
+        console.log(res);
+      },
+      error(error) {
+      console.log(error);
+      }
+    });
+  }
+
+  // Delete employee 
+  deleteEmployee(empId:number){
+    const isDelete = confirm("Are you sure ?");
+    if(isDelete){
+      this.masterService.deleteEmployee(empId).subscribe({
+        next:(res)=> {
+     console.log('Employee deleted successfully !');
+       this.getAllEmployee();
+        },
+        error:(error)=>{
+         console.log("Failed to delete employee", error);
+        },
+        complete(){
+        }
+      })
+    }
+
+  }
+
+
+  //Edit employee
+  editEmployee(empObj:Employee){
+    console.log(empObj);
+    this.employeeObj = empObj;
+    this.isFormVisible.set(true);
+  }
 }
--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { IApiResponse } from '../model/interface/master';
 import { Observable } from 'rxjs';
 import { IsActiveMatchOptions } from '@angular/router';
+import { Employee } from '../model/class/Employee';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,10 +15,30 @@ export class MasterService {
     
 
   getAllParentDepartment():Observable<IApiResponse>{
-    return this.http.get<IApiResponse>(`${this.apiUrl}+"/GetParentDepartment`);
+    return this.http.get<IApiResponse>(`${this.apiUrl}/GetParentDepartment`);
   }
+
   getAllChildDepartmentById(departmentId:number):Observable<IApiResponse>{
+    alert(departmentId);
   return this.http.get<IApiResponse>(`${this.apiUrl}/GetChildDepartmentByParentId?deptId=${departmentId}`);
+  }
+
+
+  saveEmployee(data:Employee):Observable<IApiResponse>{
+   return this.http.post<IApiResponse>(`${this.apiUrl}/CreateEmployee`,data);
+  }
+
+  getAllEmployee():Observable<Employee[]>{
+    return this.http.get<Employee[]>(`https://projectapi.gerasim.in/api/EmployeeManagement/GetAllEmployees`);
+  }
+
+  deleteEmployee(empId:number){
+    return this.http.delete(`${this.apiUrl}/DeleteEmployee/${empId}`);
+  }
+
+  updateEmployee(empObj:Employee):Observable<Employee>{
+    return this.http.put<Employee>(this.apiUrl+"/UpdateEmployee/"+empObj.employeeId,empObj);
+
   }
   
 
